49f43c68385918dea601c7913f6caa7ca8bf5891
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-11-03 20:57:21 +0530

    Created App bar and hero Card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar/NavBar";
+import HeroCard from "./components/Hero/HeroCard";
+import logo from './Assets/Avatar.png';
+import dish from './Assets/foodDish.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar logo={logo} />
+      <div className="container d-flex f-wrap">
+        <HeroCard avatar={logo} logo={dish} />
+        <HeroCard avatar={logo} logo={dish} />
+        <HeroCard avatar={logo} logo={dish} />
+      </div>
     </div>
   );
 }
